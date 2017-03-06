3bf8b975128722f4a09f04a20723c171f7693674
Anh Luong <anhluong019@gmail.com>
2017-03-05 21:40:27 -0800

    Question 1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
 
 	include CaseTabsMethods
 	def index
+		@q1 = "{key:[[value_1, value_2],[value_3, value4]], 5:10:00AM]}"
+		@expect_a1 = {"key" => [["value_1", "value_2"],["value_3", "value4"]], 5=>"10:00AM"}
+		@a1 = format_hash(@q1)
+
 		@q2 = {"this" => [1,2,3,4,5,6], "that" => ['here', 'there', 'everywhere'], :other => 'here'}
 		@a2 = Sample.new(@q2)
 
--- lib/case_tabs_methods.rb
+++ lib/case_tabs_methods.rb
@@ -1,5 +1,71 @@
 module CaseTabsMethods
 
+	def format_hash(h)
+		finding_key = true
+		finding_val = false
+		key = ''
+		val = ''
+		last_key = ''
+		last_val = ''
+		unclosed_count = 0
+		closed_count = 0
+		hash = {}
+		arr = []
+		h.each_char.with_index do |c, i|
+			# If is a letter or digit or underscore
+			if (c =~ /[A-Za-z0-9_]/) == 0
+
+				# Previous char is a symbol
+				if (h[i-1] =~ /[A-Za-z0-9_]/) != 0 && h[i-1] != '[' && h[i-1] != ']'
+					last_key = key
+					last_val = val
+					key = ''
+					val = ''
+				end
+				key.concat(c) if finding_key
+				val.concat(c) if finding_val
+
+			# If is a symbol
+			else
+				if c == '['
+					if finding_val
+						unclosed_count += 1
+					end
+				elsif c == ']'
+					if finding_val
+						closed_count += 1
+					end
+				end
+				# Previous char is a letter or digit or underscore
+				if (h[i-1] =~ /[A-Za-z0-9_]/) == 0
+					if finding_key
+						finding_key = false
+						finding_val = true
+						hash[key] = val
+					else
+						if unclosed_count > 0
+							if unclosed_count > closed_count
+								arr << val
+							elsif unclosed_count == closed_count
+								finding_key = true
+								finding_val = false
+								unclosed_count = 0
+								closed_count = 0
+								hash[last_key] = arr
+								arr = []
+							end
+						else
+							finding_key = true
+							finding_val = false
+							hash[last_key] = val
+						end
+					end
+				end
+			end
+		end
+		return hash
+	end
+
 	class Sample
 
 		def initialize(arbitrary_hash)
